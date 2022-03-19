f864ee0c71482bfffd0b85d70150f024efd3bbbf
Johannes Maas <github@j-maas.de>
2022-03-19 21:09:56 +0100

    Simplify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
     build: {
-        outDir: "dist",
         rollupOptions: {
             input: "index.html",
         },
